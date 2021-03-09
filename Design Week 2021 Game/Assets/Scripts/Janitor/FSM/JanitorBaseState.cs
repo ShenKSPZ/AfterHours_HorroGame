8f046b545a1ea63d8b44ee8db1a845b56de4d4ec
Zihe Zhang <zhangzihezealf@gmail.com>
2021-03-09 17:36:34 -0500

    FSM base setup

---
+++ Design Week 2021 Game/Assets/Scripts/Janitor/FSM/JanitorBaseState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JanitorBaseState : FSMBaseState<JanitorFSM>
+{
+    protected JanitorController controller;
+    protected Animator janitorAnimator;
+    public override void Init(GameObject _owner, FSM _fsm)
+    {
+        base.Init(_owner, _fsm);
+        controller = owner.GetComponent<JanitorController>();
+        Debug.Assert(controller != null, $"{owner.name} requires a JanitorController Component");
+
+        janitorAnimator = owner.GetComponent<Animator>();
+        Debug.Assert(janitorAnimator != null, $"{owner.name} requires an Animator Component");
+    }
+}
---
+++ Design Week 2021 Game/Assets/Scripts/Janitor/FSM/JanitorFSM.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(JanitorController))]
+public class JanitorFSM : FSM
+{
+    public readonly int IdleState = Animator.StringToHash("Idle");
+    public readonly int PatrolState = Animator.StringToHash("Patrol");
+    public readonly int AlertState = Animator.StringToHash("Alert");
+    public readonly int ChaseState = Animator.StringToHash("Chase");
+}
