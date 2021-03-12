c81132194365a40f784a2e11388f8f1c72680ddc
Lawrence <2793500992@qq.com>
2021-03-11 21:53:16 -0500

    Add Footage

---
+++ Design Week 2021 Game/Assets/Walking.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour
+{
+    public void Anim_Walk()
+    {
+        Debug.Log("Walk");
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Janitor/Walk", GetComponent<Transform>().position);
+    }
+}
