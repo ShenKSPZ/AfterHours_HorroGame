730810640629b63fa0e0bfe6f27baeb5aff8c4a4
Lawrence <2793500992@qq.com>
2021-03-12 02:00:48 -0500

    Add game start

---
+++ Design Week 2021 Game/Assets/GotoScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Framework;
+
+public class GotoScene : MonoBehaviour
+{
+    public void GOTO(string name)
+    {
+        ScenesMgr.I().LoadSceneAsyn(name, () => { }, (x) => { });
+    }
+}
